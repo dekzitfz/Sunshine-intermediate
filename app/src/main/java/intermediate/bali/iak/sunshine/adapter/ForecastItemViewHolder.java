e76b4d87293a14629b540294545dba423d173ef4
Adi Andrea <dekz.itfz@gmail.com>
2017-05-07 11:37:34 +0800

    create viewholder

---
+++ app/src/main/java/intermediate/bali/iak/sunshine/adapter/ForecastItemViewHolder.java
@@ -0,0 +1,36 @@
+package intermediate.bali.iak.sunshine.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import intermediate.bali.iak.sunshine.R;
+import intermediate.bali.iak.sunshine.model.DummyForecast;
+
+/**
+ * Created by DEKZ on 5/7/2017.
+ */
+
+public class ForecastItemViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.img_weather)ImageView img;
+    @BindView(R.id.tv_day)TextView day;
+    @BindView(R.id.tv_weatherDesc)TextView weatherDesc;
+    @BindView(R.id.tv_maxTemp)TextView maxTemp;
+    @BindView(R.id.tv_minTemp)TextView minTemp;
+
+    public ForecastItemViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(itemView);
+    }
+
+    public void bind(DummyForecast data){
+        day.setText(data.getDay());
+        weatherDesc.setText(data.getWeather());
+        maxTemp.setText(data.getMaxTemp());
+        minTemp.setText(data.getMinTemp());
+    }
+}
