379d30ba82ae9d437a888691423074d6e2d6286f
Adi Andrea <dekz.itfz@gmail.com>
2017-05-13 13:27:35 +0800

    add detail activity

---
+++ app/src/main/java/intermediate/bali/iak/sunshine/DetailActivity.java
@@ -0,0 +1,63 @@
+package intermediate.bali.iak.sunshine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by DEKZ on 5/13/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+
+    @BindView(R.id.detail_date)
+    TextView date;
+    @BindView(R.id.detail_high_temperature)
+    TextView highTemp;
+    @BindView(R.id.detail_low_temperature)
+    TextView lowTemp;
+    @BindView(R.id.detail_weather_icon)
+    ImageView weatherIcon;
+    @BindView(R.id.detail_weather_description)
+    TextView weatherDesc;
+    @BindView(R.id.detail_humidity)
+    TextView humidity;
+    @BindView(R.id.detail_pressure)
+    TextView pressure;
+    @BindView(R.id.detail_wind)
+    TextView wind;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        NavUtils.navigateUpFromSameTask(this);
+    }
+}
